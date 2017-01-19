fb32355f379efe35d0ecd72b527e49d190be0fbb
Sagi Shutrit <contact@sagishutrit.com>
2017-01-19 16:00:53 +0100

    removed content

--- send_html_images.php
+++ send_html_images.php
@@ -9,22 +9,23 @@ try {
             ->setSubject('News Letter September')
             ->setFrom($from)
       
-            ->addTo('luther@domain.nl','title of your first message')
-            ->addTo('user@gmail.com','title other message')
-            ->addTo('name3@domain.com','title of your third message');
+            ->addTo('contact@sagishutrit.com','shasha haganuv1')
+            ->addTo('sags2012@gmail.com','shasha haganuv2')
+            ->addTo('shutrit@hotmail.com','count shugulla city');
             
             // embeding using the swift mailer library 
+            
+            
             $image = $message->embed(Swift_Image::fromPath('images/Diana_cracked.jpg'));
-            $logo =  $message->embed(Swift_Image::fromPath('images/logo.jpg'));
+            $logo =  $message->embed(Swift_Image::fromPath('images/shutritartandesign.jpg'));
            
             $html = <<<EOT
 <!DOCTYPE html>
 <html lang="en">
-<head><title>September </title>
+<head><title>News letter September</title>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <style type="text/css">
 body {
-    
     padding:0;
     margin:0;
 }
@@ -33,24 +34,18 @@ img {
     max-width:100%;
 }
 #container {
-        margin: 0 auto;
-        position:relative;
-        background-color:#fff;
-        width:70%;
-        min-height:500px;
-        padding:2px;
-        
-    
+    margin: 0 auto;
+    position:relative;
+    background-color:#fff;
+    width:70%;
+    min-height:500px;
+    padding:2px;
 }
 #container h1 {
     margin: 0 auto;
     text-align:center;
     color:#00b3b3;
 }
-
-
-
-
 </style>
 </head>
 <body>
@@ -61,22 +56,10 @@ img {
 <tr>
 <td style="width:55%;">
 <h2 style="color:#00b3b3;">Restoring Diana</h2>
-<p>In a  rather dramatic incident the portriat of lady Diana in Scallywags Restaurant The Hague was damaged.
- A crack in the area of the face and neck was created. Shutrit Art & Design  gladly accepted the commission of restoring the painting.<br>
- Sagi and Richard have been enjoying a fruitful  artist and patron relationship for a number of years.  
- The challenge will be to re-complete the canvas after the meticulous work of Janine Hilverdink who excuted this beatiful portrait in 2004
-Shutrit Art and design is plannig to tackle this challenge by applying an extra layer to defuse the line created by the crack. </p>
-</td>
-<td style="width:45%;">
-<figure>
- <img src="$image" alt="Painting Lady Diana">
-<figcaption>Lady Diana</figcaption>   
-</figure>
-</td>
-</tr>
-<tr><td colspan=2>
-  <center>
-<p>Wishing You a productive month<br></p>
+<p>In a  dramatic incident the portriat of lady Diana in the Scallywags Restaurant The Hague was damaged.
+ A crack in the area of the face and neck was created. Shutrit Art & Design  gladly accepted the commission of restoring the painting.</p></td><td style="width:45%;"><figure> <img src="$image" alt="Painting Lady Diana">
+<figcaption>Lady Diana</figcaption>   </figure></td></tr><tr><td colspan=2><center><p>Wishing You a productive month
+<br></p>
 <a href="http://www.sagishutrit.com">
 <img src="$logo" alt="logo">
 </a>
@@ -90,31 +73,23 @@ Shutrit Art and design is plannig to tackle this challenge by applying an extra 
 EOT;
             
             $message->setBody($html, 'text/html');
-       
-            
- 
-       
             $transport = Swift_SmtpTransport::newINstance($smtp_server,465, 'ssl')
-              ->setUsername($username)
-              ->setPassword($password);
-              
-              
-              $mailer = Swift_Mailer::newInstance($transport); 
-              $result= $mailer->send($message);
+            ->setUsername($username)
+            ->setPassword($password);
+            $mailer = Swift_Mailer::newInstance($transport); 
+            $result= $mailer->send($message);
                
-               if($result) {
+            if($result) {
                   echo'Number Of emails Send :'. $result;
-              } else {
+            } else {
                   echo"could not send Email";
-              }
+            }
               
         
               
 }
 catch (Exception $e) {
-    
-    
-   echo $e->getMessage();
+    echo $e->getMessage();
 }
 
    
